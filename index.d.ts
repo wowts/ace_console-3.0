8e7596b24455f6e59b74b740b7fede8ec427edfb
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 12:51:27 +0100

    v1.1.0

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Library } from "@wowts/tslib";
-export interface AceConsole {
-}
-declare const lib: Library<AceConsole>;
-export default lib;
--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const lib = {
-    Embed(Base) {
-        return class extends Base {
-        };
-    },
-};
-exports.default = lib;
